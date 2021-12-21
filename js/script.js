4ab70ec27ae1821a4f17b73322e63c4e1160ae98
Juan Goncalves Patrao <goncalvespatraoj@gmail.com>
2021-12-21 02:24:16 -0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// inicio con nombre
+let nombre = prompt("Escribe tu nombre");
+
+console.log("Que onda " + nombre);
+
+// suma
+let valor1 = prompt("Dame el primer valor");
+let valor2 = prompt("Dame el segundo valor");
+
+let suma = parseInt(valor1) + parseInt(valor2);
+
+var resultado = alert("El resultado de tu suma es " + suma);
+
+console.log("El resultado de tu suma es " + suma);
+
+// concatenacion
+let dia = prompt("Ingresa un dia de la semana");
+let juego = prompt("Ingresa tu juego favorito");
+
+var concatenacion = alert(nombre + " juega todo el " + dia + " " + juego);
+
+console.log(nombre + " juega todo el " + dia + " " + juego);
